a4010595a6f59c05f1351938f4ef916e270175da
NickUA <246246s@gmail.com>
2023-06-13 23:35:14 +0300

    refactor: separate const to config and isExist to util function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,15 +1,15 @@
 import * as fs from 'node:fs/promises';
 import path from 'node:path';
 import * as url from 'node:url';
+import isExist from './utils/check-exist.js';
+import { SOURCE_FOLDER_RELATIVE_PATH, DEST_FOLDER_RELATIVE_PATH } from './utils/config.js';
 
 const modulePath = url.fileURLToPath(new URL(import.meta.url));
 const __dirname = path.dirname(modulePath);
-const sourceRelativePath = './files';
-const destinationRelativePath = './files_copy';
 
 const copy = async () => {
-  const sourcePath = path.join(__dirname, sourceRelativePath);
-  const destinationPath = path.join(__dirname, destinationRelativePath);
+  const sourcePath = path.join(__dirname, SOURCE_FOLDER_RELATIVE_PATH);
+  const destinationPath = path.join(__dirname, DEST_FOLDER_RELATIVE_PATH);
 
   if (!(await isExist(sourcePath))
     || await isExist(destinationPath)) throw new Error('FS operation failed');
@@ -27,12 +27,6 @@ const realCopy = async (sourceFolder, destinationFolder) => {
     if (file.isFile()) fs.copyFile(source, dest);
     else realCopy(source, dest);
   }
-};
-
-const isExist = async (path) => {
-  return await fs.access(path)
-    .then(() => true)
-    .catch(() => false);
 };
 
 await copy();
