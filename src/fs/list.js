702359dcb712e7b3ba5d1b652d57d2388fc0aea5
NickUA <246246s@gmail.com>
2023-06-14 00:00:14 +0300

    feat: implement delete file

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import * as fs from 'node:fs/promises';
+// import path from 'node:path';
+import isExist from './utils/check-exist.js';
+import { SOURCE_FOLDER_RELATIVE_PATH } from './utils/config.js';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = new URL(SOURCE_FOLDER_RELATIVE_PATH, import.meta.url);
+    if (!(await isExist(folderPath))) throw new Error('FS operation failed');
+
+    const files = await fs.readdir(folderPath);
+    // for (const file of files) console.log
+    console.table(files);
+
+
 };
 
 await list();
