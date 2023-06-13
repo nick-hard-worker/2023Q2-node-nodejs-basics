26ddd48e78ed6ef288cc0059d265c1976dab1e8f
NickUA <246246s@gmail.com>
2023-06-14 00:34:15 +0300

    feat: implement cli env task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,7 @@
 const parseEnv = () => {
-    // Write your code here 
+    const allEnv = Object.entries(process.env);
+    const filteredEnv = allEnv.filter(item => /^RSS_/.test(item[0]));
+    for (const variable of filteredEnv) console.log(`${variable[0]}=${variable[1]}`);
 };
 
 parseEnv();
