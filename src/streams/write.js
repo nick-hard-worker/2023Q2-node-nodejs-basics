1e5ebf18dbb9c58b87ad8e9a106790d8844c3536
NickUA <246246s@gmail.com>
2023-06-16 22:52:35 +0300

    feat: implement writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,10 @@
+import { createWriteStream } from 'node:fs';
+const outputFile = './files/fileToWrite.txt';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = new URL(outputFile, import.meta.url);
+    const outputStream = createWriteStream(filePath);
+    process.stdin.pipe(outputStream);
 };
 
 await write();
